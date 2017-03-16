f7a449fa0755b85a1bd9b16a3e270e9b8a855186
Nan Zhu <zhunansjtu@gmail.com>
2017-03-16 15:04:18 -0700

    configure jobs

--- src/main/scala/spark/RandomLettersGenerator.scala
+++ src/main/scala/spark/RandomLettersGenerator.scala
@@ -8,10 +8,11 @@ object RandomLettersGenerator {
   def main(args: Array[String]): Unit ={
     val sc = new SparkContext()
     val partitions = args(0).toInt
+    val partitionSize = args(1).toInt
     sc.parallelize((0 until partitions).toList, partitions).mapPartitions {
       _ =>
         val list = new ListBuffer[Int]
-        for (i <- 0 until 100000) {
+        for (i <- 0 until partitionSize) {
           list += i
         }
         list.iterator
